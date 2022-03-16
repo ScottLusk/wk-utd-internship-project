f33538d9c325b225b3aefeff66b1efb32ba98182
Toshal Tambave <toshal.tambave@wolterskluwer.com>
2022-03-16 11:14:49 -0500

    Creating account-details & show component, adding service

--- src/front-end/src/app/app.module.ts
+++ src/front-end/src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccountDetailsComponent } from './account-details/account-details.component';
+import { ShowComponent } from './account-details/show/show.component';
+import { WebapiService } from './webapi.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountDetailsComponent,
+    ShowComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [WebapiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
