46900264dc491032c9eeda2a0ea1335ce1bd33ad
Toshal Tambave <toshal.tambave@wolterskluwer.com>
2022-03-16 13:04:02 -0500

    sample routing

--- src/front-end/src/app/app-routing.module.ts
+++ src/front-end/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountDetailsComponent } from './account-details/account-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'details', component:AccountDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
